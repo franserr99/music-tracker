fbd66e13fefc676c6acf78b4df9b00a0b916da50
francisco laptop <serrfrancisco@yahoo.com>
2023-11-12 20:42:35 -0800

    finishing extraction

---
+++ client/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <footer style={{ backgroundColor: '#f8f9fa', padding: '10px 0', textAlign: 'center' }}>
+        <p>Copyright &copy; {new Date().getFullYear()} My Website</p>
+      </footer>
+    );
+  };
+export default Footer;
---
+++ client/components/ui/Button.tsx
@@ -0,0 +1,17 @@
+// Button.tsx
+'use client';
+interface ButtonProps {
+    text: string;
+    onClick: () => void; 
+  }
+  
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+    return (
+        <button onClick={onClick}>
+        {text}
+        </button>
+    );
+};
+  
+export default Button;
+  
